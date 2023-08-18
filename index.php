7957991761a0107129269063bf3871ea2d26920d
mitesh1622 <139164767+mitesh1622@users.noreply.github.com>
2023-08-18 12:28:16 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Servico</title>
+	<style>
+		body {
+			font-family: "Helvetica Neue", Arial, sans-serif;
+			background-color: grey;
+			margin: 0;
+			padding: 0;
+			
+		}
+
+		h1 {
+			text-align: center;
+			color: #333;
+			margin-top: 30px;
+		}
+
+		ul {
+			list-style-type: none;
+			margin: 0;
+			padding: 0;
+			display: flex;
+			justify-content: center;
+			background-color: #4CAF50;
+			padding: 10px 0;
+			box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+			border-radius: 10px;
+		}
+
+		li {
+			margin: 5px;
+		}
+
+		li a {
+			display: inline-block;
+			margin: auto;
+			padding: 8px;
+			border-radius: 10px;
+			text-decoration: none;
+
+
+		}
+
+		li a:hover {
+			background-color: #45a049;
+			color: black;
+			text-decoration: none;
+			border-radius: 10px;
+			font-size: 18px;
+			transition: background-color 0.3s ease;
+			background-color: grey;
+		}
+	</style>
+
+
+</head>
+
+<body>
+	<h1 align="center">WELCOME TO SERVICO</h1><br><br>
+	<ul>
+
+		<li><a href="home.php">Home</a></li>
+		<li><a href="customer.php">Customers</li>
+		<li><a href="employee.php">Employees</li>
+		<li><a href="teamservico.php">Team Servico</li>
+		<li><a href="admin.php">Admin</li>
+
+	</ul>
+</body>
+
+</html>
